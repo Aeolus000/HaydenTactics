54de0c1b843fc2654cf349baa5640fc7c499db4e
Aeolus000 <HaydenCraig000@gmail.com>
2024-09-11 20:38:08 -0400

    GUI displays unit

--- GUITest.py
+++ GUITest.py
@@ -13,19 +13,28 @@ class UnitGUI(QWidget):
         super().__init__()
         #self.setGeometry(700, 300, 500, 500)
         self.list1 = QListWidget(self)
+        self.unit_stats = QLabel("Placeholder", self)
         self.button = QPushButton("Create Random Character", self)
+        self.exit = QPushButton("Exit", self)
 
         self.initUI()
+        
+
 
     def initUI(self):
 
         self.setWindowTitle("Hayden Tactics")
 
+        self.is_running = True
+
         grid = QGridLayout()
         grid.addWidget(self.list1)
         grid.addWidget(self.button)
+        grid.addWidget(self.unit_stats)
+        grid.addWidget(self.exit)
 
         self.list1.setGeometry(0, 0, 200, 200)
+        self.unit_stats.setGeometry(0, 0, 500, 500)
         #self.button.setGeometry(0, 200, 100, 100)
         #self.button.setStyleSheet("font-size: 25px;"
         #                          "font-family: Arial")
@@ -33,16 +42,53 @@ class UnitGUI(QWidget):
 
         self.setLayout(grid)
 
-        self.button.clicked.connect(self.selected_item)
+        self.button.clicked.connect(self.display_random_unit)
 
-    def selected_item(self):
+        #self.list1.itemSelectionChanged.connect(self.selection_changed)
+        self.list1.itemClicked.connect(self.selection_changed)
+
+
+
+    def selection_changed(self):
+
+        unit = self.get_selected_unit()
+
+        unit_stats = Unit.display_unit(unit)
+
+        self.unit_stats.setText(unit_stats)
+
+
+    def get_selected_unit(self):
+
+        item = self.list1.currentItem()
+
+        unit_id = int(item.text().split(' ')[0])
+        unit = Unit.get_unit_by_id(unit_id)
+
+        return unit
+        
+
+
+
+    def display_random_unit(self):
         unit = Unit.generate_random_unit()
-        print(f"created {unit.name}")
+        #print(f"created {unit.name}")
 
         self.list1.clear()
 
         for unit in Unit.unitlist:
+            #item = QListWidgetItem(unit.id, unit.name)
+            #self.list1.addItem(item)
             self.list1.addItem(f"{unit.id} {unit.name}")
+
+            
+
+        #self.unit_stats.text("Blah")
+
+        
+
+    #def display_unit_stats(self):
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- StatsTest.py
+++ StatsTest.py
@@ -37,3 +37,42 @@ levelup_stats = {
         'baseResistance': 2,
                     },
                 }                    
+
+
+
+
+# def display_unit(unit, option = 0):
+#     print("\n************************\n")
+#     print(f"ID: {unit.id}\tName:\t{unit.name}")
+#     print(f"Job:\t{unit.charclass}")
+#     print(f"Level:\t{unit.level}")
+
+#     if option == 1:
+#         print(f"HP: {unit.currentHP}/{unit.maxHP} Mana: {unit.currentMana}/{unit.maxMana}")
+#         if unit.weaponslot1:
+#             print(f"EQUIP: {unit.weaponslot1.name}")
+#             print("\n************************")
+#         if unit.weaponslot2:
+#             print(f"EQUIP: {unit.weaponslot2.name}")
+#             print("\n************************")
+#         if unit.weaponslot3:
+#             print(f"EQUIP: {unit.weaponslot3.name}")
+#             print("\n************************")
+              
+#     if option == 2:  
+#         print("************************")
+#         print(f"Current / Max HP:\t{unit.currentHP} / {unit.maxHP}")
+#         print(f"Current / Max Mana:\t{unit.currentMana} / {unit.maxMana}")
+#         print(f"Strength:\t{unit.baseStrength}")
+#         print(f"Dexterity:\t{unit.baseDexterity}")
+#         print(f"Speed:\t\t{unit.baseSpeed}")
+#         print(f"Vitality:\t{unit.baseVitality}")
+#         print(f"Constitution:\t{unit.baseConstitution}")
+#         print(f"Intelligence:\t{unit.baseIntelligence}")
+#         print(f"Mind:\t\t{unit.baseMind}")
+#         print(f"Resistance:\t{unit.baseResistance}")
+#         print(f"You would deal {get_melee_damage(unit)} melee damage.")
+#         print(f"You have a {unit.melee_hit_chance}% chance to hit.")
+#         print(f"You have {unit.basePhysicalResistance}% Physical Resistance.")
+#         if unit.weaponslot1:
+#             print(f"You have a {unit.weaponslot1.name} equipped.")
